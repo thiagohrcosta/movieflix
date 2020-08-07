5958c5d912e90fa7413f54112dfc5ae925807cd7
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-08-07 19:58:17 -0300

    Atualizando Heroku

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-const URL_BACKEND = window.location.hostname === 'localhost' ? 'http://localhost:8080' : 'https://movietrailer-alura.herokuapp.com';
+const URL_BACKEND = window.location.hostname === 'localhost' ? 'http://localhost:8080' : 'https://app-movieflix.herokuapp.com/';
    
 export default{ URL_BACKEND };
